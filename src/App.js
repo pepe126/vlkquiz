fb0554d204a4a318b3089b97d0e48c75c919470e
Pepe <tullioangius@icloud.com>
2020-12-04 18:44:15 +0100

    ristrutturato componente Game per facilitare il reset delle domande

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './styles/App.css';
 import {Route, BrowserRouter as Router, Link} from 'react-router-dom';
 import Home from './pages/Home';
 import Istruzioni from './pages/Istruzioni';
@@ -10,48 +10,18 @@ import WinPage from './pages/WinPage'
 
 
 function App() {
-  const[questions, setQuestions] = useState(null)
-  const[answers, setAnswers] = useState(null)
-
 
   useEffect(() => {
-      getQuestions();
-      window.addEventListener("popstate", () => {
-        getQuestions();
-    });
+    
   
   },[]);
- 
-
-
-  function shuffleArray(array) {
-      let i = array.length - 1;
-      for (; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        const temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
-      }
-      return array;
-  }
-
-  const getQuestions = async () => {
-      const response = await fetch(`https://opentdb.com/api.php?amount=10&category=18&difficulty=medium&type=multiple`);
-      const questionsData = await response.json()
-      setQuestions(questionsData.results);
-      setAnswers(questionsData.results.map((data) => shuffleArray(data.incorrect_answers.concat(data.correct_answer))));
-      console.log(questionsData.results);
-  }
 
   return (
     <Router>
       <div className="App">
         <Route path = "/" exact render = {() => <Home />}/>
         <Route path = "/istruzioni" render = {() => <Istruzioni /> }/>
-        {questions && answers && (<Route path = "/game" render = {() => <Game
-          questions = {questions}
-          answers = {answers}
-        />}/>)}
+        <Route path = "/game" render = {() => <Game/>}/>
         <Route path = "/LostPage" render = {() => <LostPage/>} />
         <Route path = "/WinPage" render = {() => <WinPage/>} />
       </div>
--- src/pages/Game.js
+++ src/pages/Game.js
@@ -6,16 +6,23 @@ import { useEffect, useState } from 'react'
 import Infographic from '../components/Infographic'
 
 
-const Game = (props) => {
+const Game = () => {
 
     const history = useHistory()
     const[numeroDomanda, setNumeroDomanda] = useState(0);
-    const [time, setTime] = useState(20)
+    const [time, setTime] = useState(21)
+    const[questions, setQuestions] = useState(null)
+    const[answers, setAnswers] = useState(null)
+
 
     useEffect(() => {
         const interval = setInterval(() => {
           updateTime();
         }, 1000);
+        
+        if(time == 21){
+            getQuestions();
+        }
         
         window.addEventListener("popstate", () => {
             history.push("/");
@@ -25,6 +32,26 @@ const Game = (props) => {
           clearInterval(interval);
         };
       }, [time])
+
+
+    function shuffleArray(array) {
+        let i = array.length - 1;
+        for (; i > 0; i--) {
+          const j = Math.floor(Math.random() * (i + 1));
+          const temp = array[i];
+          array[i] = array[j];
+          array[j] = temp;
+        }
+    return array;
+    }
+
+    const getQuestions = async () => {
+        const response = await fetch(`https://opentdb.com/api.php?amount=10&category=18&difficulty=medium&type=multiple`);
+        const questionsData = await response.json()
+        setQuestions(questionsData.results);
+        setAnswers(questionsData.results.map((data) => shuffleArray(data.incorrect_answers.concat(data.correct_answer))));
+        console.log(questionsData.results);
+    }
 
 
     function updateTime () {
@@ -44,7 +71,7 @@ const Game = (props) => {
     const giveAnswer = (e) => {
         var givenAnswer = e.target.value;
         console.log(givenAnswer)
-        if (givenAnswer == props.questions[numeroDomanda].correct_answer){
+        if (givenAnswer == questions[numeroDomanda].correct_answer){
             if(numeroDomanda == 9){
                 history.push("/WinPage")
             } else{
@@ -58,16 +85,22 @@ const Game = (props) => {
 
     return(
         <div>
+            
+            {questions && answers && (<div>
+
             <h1>{time}</h1>
             <Infographic
                 numero = {numeroDomanda + 1}
             />
-            <h2>{props.questions[numeroDomanda].question}</h2>
-            <button onClick={giveAnswer} value={props.answers[numeroDomanda][0]}>{props.answers[numeroDomanda][0]}</button>
-            <button onClick={giveAnswer} value={props.answers[numeroDomanda][1]}>{props.answers[numeroDomanda][1]}</button>
+            <h2>{questions[numeroDomanda].question}</h2>
+            <button onClick={giveAnswer} value={answers[numeroDomanda][0]}>{answers[numeroDomanda][0]}</button>
+            <button onClick={giveAnswer} value={answers[numeroDomanda][1]}>{answers[numeroDomanda][1]}</button>
                 <br/>
-            <button onClick={giveAnswer} value={props.answers[numeroDomanda][2]}>{props.answers[numeroDomanda][2]}</button>
-            <button onClick={giveAnswer} value={props.answers[numeroDomanda][3]}>{props.answers[numeroDomanda][3]}</button>
+            <button onClick={giveAnswer} value={answers[numeroDomanda][2]}>{answers[numeroDomanda][2]}</button>
+            <button onClick={giveAnswer} value={answers[numeroDomanda][3]}>{answers[numeroDomanda][3]}</button>
+        
+            </div>)}
+
         </div>
     )
 }
